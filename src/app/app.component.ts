5868a8045818627368dd26e8b924b34715a9f798
chris <chris@joycenet.us>
2021-11-29 13:30:03 -0600

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup,  FormBuilder,  Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'ngValidation';
+  title = 'My Angular Form Validation Example';
+   myForm!: FormGroup;
+   constructor(private fb: FormBuilder) {
+    this.createForm();
+  }
+   createForm() {
+    this.myForm = this.fb.group({
+       username: ['', Validators.required ]
+    });
+  }
 }
